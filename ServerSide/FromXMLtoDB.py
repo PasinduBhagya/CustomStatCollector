c1a2e67b7ba09d956376a87acfe4da7bf71a76f5
PasinduBhagya <pasindubhagyapeiries@gmail.com>
2024-06-06 18:06:21 +0530

    Code was completed need to verify

--- ServerSide/FromXMLtoDB.py
+++ ServerSide/FromXMLtoDB.py
@@ -1,11 +1,53 @@
 import requests
 import xml.etree.ElementTree as ET
+import mysql.connector
 from datetime import datetime
+from configparser import ConfigParser
 
-def addToDB():
-  # sql_query = """INSERT INTO tbx_bookings_quotes_count_reports (ProjectName, YEAR, MONTH, BOOKING_COUNT, PAX_COUNT, QUOTE_COUNT, QUOTE_PAX_COUNT) VALUES (%s, %s, %s, %s, %s, %s, %s)"""
+config = ConfigParser()
+config.read('./configurations.cfg')
+
+def createSQLQuery(tagList):
   # values = (ProjectName, YEAR, MONTH, BOOKING_COUNT, PAX_COUNT, QUOTE_COUNT, QUOTE_PAX_COUNT)
-  pass
+  TABLE_NAME = config.get('DATABASE', 'TABLE_NAME'), # Tablename should get from the Config File.
+
+  TableColumns = ("(" + ", ".join(tagList) + ")")
+  TableColumnStrings = []
+  for i in range(len(tagList)):
+    TableColumnStrings.append(f"%s")
+        
+  TableColumnString = ("(" + ", ".join(tuple(TableColumnStrings)) + ")")
+      
+  SQL_QUERY = f"INSERT INTO {TABLE_NAME} {TableColumns} VALUES {TableColumnString}"
+
+  return SQL_QUERY
+
+def connectToDatabase():
+    return mysql.connector.connect(
+        host=config.get('DATABASE', 'HOST'),
+        user=config.get('DATABASE', 'USER'),
+        password=config.get('DATABASE', 'PASSWORD'),
+        database=config.get('DATABASE', 'DATABASE'),
+    )
+
+def addtoDB(valuesDictionary, tagList, SQL_QUERY):
+  database = connectToDatabase()
+  dbcursor = database.cursor()
+  
+  valuesList = []
+  
+  for tag in tagList: # ['Year', 'MONTH', 'BOOKING_COUNT', 'PAX_COUNT', 'QUOTE_COUNT', 'QUOTE_PAX_COUNT']
+    valuesList.append(valuesDictionary[tag])
+  
+  valuesForSQLQuery = tuple(valuesList)
+  print("-"*40)
+  print(SQL_QUERY)
+  print(valuesForSQLQuery)
+  print("-"*40)
+
+  dbcursor.execute(SQL_QUERY, valuesForSQLQuery)
+  database.commit()
+  
 
 def getResponse(url):
   tagList = []
@@ -31,20 +73,40 @@ def getResponse(url):
     return None
 
 def main():
-  with open('fetchingURLs.txt', 'r') as FURLS:
+  with open('fetchingURLs.conf', 'r') as FURLS:
     for URL in FURLS:
       URL = URL.strip()
       if (len(URL) != 0):
         print("INFO: Checking " + URL)
         if (getResponse(URL) != None):
+          
           items, tagList = getResponse(URL)
+          SQL_QUERY = createSQLQuery(tagList)
+
+          # <item>
+            # <ProjectName>YAS</ProjectName>
+            # <Year>2024</Year>
+            # <MONTH>JAN</MONTH>
+            # <BOOKING_COUNT>574</BOOKING_COUNT>
+            # <PAX_COUNT>2031</PAX_COUNT>
+            # <QUOTE_COUNT>358</QUOTE_COUNT>
+            # <QUOTE_PAX_COUNT>1313</QUOTE_PAX_COUNT>
+          # </item>
+          
           for item in items:
-            for tag in tagList:
+            valuesDictionary = {}
+            for tag in tagList: # ['Year', 'MONTH', 'BOOKING_COUNT', 'PAX_COUNT', 'QUOTE_COUNT', 'QUOTE_PAX_COUNT']
                 try:
-                  print(tag + "\t" + item.find(tag).text)
+                  # print(tag + "\t" + item.find(tag).text)
+                  valuesDictionary[tag] = item.find(tag).text
                 except AttributeError:
-                  print(tag + "\t" + str(item.find(tag)))
+                  # print(tag + "\t" + str(item.find(tag)))
+                  valuesDictionary[tag] = item.find(tag)
                 except:
                   pass
-
+            addtoDB(valuesDictionary, tagList, SQL_QUERY)
+     
+      else:
+        tagList = []
+      
 main()
